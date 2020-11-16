3eb71738c50df35eb931bbd929036d3297ada283
Bijan Tabatabai <bijan311@gmail.com>
2020-11-16 13:50:52 -0600

    Add options to use normal distrobution of inputs and to randomize the numner of inputs in a net

--- bmks/canneal/rand_canneal_input.py
+++ bmks/canneal/rand_canneal_input.py
@@ -15,7 +15,60 @@ def index_to_net(i):
 
     return net
 
+def rand_input(uniform, num_nets):
+    if uniform:
+        return random.randrange(0, num_nets)
+    else:
+        mean = int(num_nets/2)
+        # Try to fit 3 standard deviations on either side of the mean
+        stdev = int(num_nets / 6)
+        rand = int(random.normalvariate(mean, stdev))
+        if rand > num_nets - 1:
+            rand = num_nets - 1
+        elif rand < 0:
+            rand = 0
+
+        return rand
+
+def calc_num_inputs(rand):
+    if rand:
+        # Use geometric distrobution with a mean of 5 to determine the number
+        # of inputs for this net
+        count = 1
+        while random.random() < 0.8:
+            count += 1
+        return count
+    else:
+        # If we don't want a random number of inputs, just use 5
+        return 5
+
+# Get the number of nets to use
 num_nets = int(sys.argv[1])
+
+# If there is a second argument, get the distrobution
+if len(sys.argv) >= 3:
+    if sys.argv[2] == "--dist_uniform":
+        dist_uniform = True
+    elif sys.argv[2] == "--dist_normal":
+        dist_uniform = False
+    else:
+        print("Invalid second argument. Use either --dist_uniform or --dist_normal")
+        sys.exit()
+else:
+    dist_uniform = True
+
+# If there is a third argument, determine if there should be a random number
+# of inputs per net
+if len(sys.argv) >= 4:
+    if sys.argv[3] == "--rand_num_inputs":
+        rand_num_inputs = True
+    else:
+        print("Invalid third argument. Either use --rand_num_inputs or do not "
+              "include third argument")
+        sys.exit()
+else:
+    rand_num_inputs = False
+
 # This is kind of arbitrary, but seems to matchup with what's done in the parsec inputs
 size = int(sqrt(num_nets))
 size = size - (size % 100)
@@ -26,16 +79,19 @@ print(str(num_nets) + " " + str(size) + " " + str(size))
 
 #Print the nets
 for i in range(num_nets):
+    used_nets = [i]
+
+    num_inputs = calc_num_inputs(rand_num_inputs)
+
     # Start with the net id and the "type" which is unused
     line = index_to_net(i) + " 1 "
 
     # Then add on the inputs to the net
-    for j in range(5):
-        used_nets = [i]
+    for j in range(num_inputs):
         # Make sure the inputs aren't itself or a prebious input
-        rand = random.randrange(0, num_nets)
+        rand = rand_input(dist_uniform, num_nets)
         while rand in used_nets:
-            rand = random.randrange(0, num_nets)
+            rand = rand_input(dist_uniform, num_nets)
 
         line += index_to_net(rand) + " "
         used_nets.append(rand)
